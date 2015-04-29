0e30f43a207ef4e2ea0a39ce9ea723dd363ec137
Páidí Creed <paidi@swiftkey.net>
2015-04-29 19:06:07 +0100

    Make things consistent

--- GaussianKLCriterion.lua
+++ GaussianKLCriterion.lua
@@ -1,4 +1,6 @@
 -- A criterion for computing the KL-divergence between a Gaussian and N(0,I)
+-- We assume the Gaussian is presented as a pair of vectors, one for the mean
+-- and one for the standard deviation
 -- See Kingma and Welling (http://arxiv.org/pdf/1312.6114v10.pdf) Appendix B
 
 require 'nn'
@@ -13,7 +15,7 @@ end
 
 function GaussianKLCriterion:updateOutput(input)
    local mean = input[1]
-   local variance = input[2]
+   local variance = torch.pow(input[2],2)
    
    if not self.divergences then
       self.divergences = torch.log(variance):add(1)
@@ -29,17 +31,19 @@ function GaussianKLCriterion:updateOutput(input)
 end
 
 function GaussianKLCriterion:updateGradInput(input, _)
+   local mean = input[1]
+   local stddev = input[2]
    
-   if #self.gradInput == 0 or self.gradInput[1]:size() ~= input[1]:size() then
-      self.gradInput[1] = input[1]:clone():mul(-1)
-      self.gradInput[2] = torch.ones(input[2]:size()) * -0.5
+   if #self.gradInput == 0 or self.gradInput[1]:size() ~= mean:size() then
+      self.gradInput[1] = mean:clone():mul(-1)
+      self.gradInput[2] = stddev:clone():mul(-1)
    else
-      self.gradInput[1]:copy(input[1]):mul(-1)
-      self.gradInput[2]:ones():mul(-0.5)
+      self.gradInput[1]:copy(mean):mul(-1)
+      self.gradInput[2]:copy(stddev):mul(-1)
    end
    
-   -- And the log term for the variance
-   self.gradInput[2]:add(0.5, torch.ones(input[2]:size()):cdiv(input[2]))
+   -- And the log term for the standard deviation
+   self.gradInput[2]:add(torch.ones(stddev:size()):cdiv(input[2]))
    
    return self.gradInput
 end
